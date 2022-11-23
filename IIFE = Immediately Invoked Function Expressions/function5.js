41a8fef72d588a9a0d62385344f1245572eb400f
goranc89 <114668056+goranc89@users.noreply.github.com>
2022-11-23 17:25:17 +0100

    Update function5.js

--- IIFE = Immediately Invoked Function Expressions/function5.js
+++ IIFE = Immediately Invoked Function Expressions/function5.js
@@ -6,6 +6,7 @@ Output: 28
 var octalNumber = 034;
 
 var firstFunction = function () {
+  // Ista stvar, imenovanje parametara. Ovako kad pogledas kood, da li bi ti razumeo sta ti je slovo 'o'? :) 
   return function (o) {
     return parseInt(o);
   }
